57b67a74002e391a47240974e29c57bbabb1d7da
Jake Elsberry <jelsberry96@gmail.com>
2023-12-13 15:30:47 -0800

    add form with validation for sign up page

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import Navbar from "./scenes/global/Navbar";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
+import Form from "./scenes/form";
+// import Calendar from "./scenes/calendar/calendar"
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar/calendar"
+
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -31,7 +32,7 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              {/* <Route path="/form" element={<Form />} /> */}
+              <Route path="/form" element={<Form />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
